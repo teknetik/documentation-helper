5a7fae0bf737ab2555ec021823852907303a628c
teknetik <draper.carl@gmail.com>
2023-07-22 13:15:44 +0100

    orders upsert db

--- tools/amazon/dbOps.py
+++ tools/amazon/dbOps.py
@@ -9,8 +9,7 @@ from sqlalchemy import (
     Float,
     DateTime,
 )
-from datetime import datetime
-import pandas as pd
+
 from keys import DB_USER, DB_PASSWORD
 
 # Define your PostgreSQL connection parameters
@@ -26,67 +25,82 @@ engine = create_engine(
 )
 metadata = MetaData()
 
-# Define table
-inventory = Table(
-    "inventory",
-    metadata,
-    Column("sku", String),
-    Column("fnsku", String),
-    Column("asin", String),
-    Column("product_name", String),
-    Column("condition", String),
-    Column("your_price", Integer),
-    Column("mfn_listing_exists", Boolean),
-    Column("mfn_fulfillable_quantity", Integer),
-    Column("afn_listing_exists", Boolean),
-    Column("afn_warehouse_quantity", Integer),
-    Column("afn_fulfillable_quantity", Integer),
-    Column("afn_unsellable_quantity", Integer),
-    Column("afn_reserved_quantity", Integer),
-    Column("afn_total_quantity", Integer),
-    Column("afn_inbound_working_quantity", Integer),
-    Column("afn_inbound_shipped_quantity", Integer),
-    Column("afn_inbound_receiving_quantity", Integer),
-    Column("afn_researching_quantity", Integer),
-    Column("afn_reserved_future_supply", Integer),
-    Column("afn_future_supply_buyable", Integer),
-    Column("per_unit_volume", Float),
-    Column("last_updated", DateTime),
-)
+def create_inventory():
+    # Define table
+    inventory = Table(
+        "inventory",
+        metadata,
+        Column("sku", String),
+        Column("fnsku", String),
+        Column("asin", String),
+        Column("product_name", String),
+        Column("condition", String),
+        Column("your_price", Integer),
+        Column("mfn_listing_exists", Boolean),
+        Column("mfn_fulfillable_quantity", Integer),
+        Column("afn_listing_exists", Boolean),
+        Column("afn_warehouse_quantity", Integer),
+        Column("afn_fulfillable_quantity", Integer),
+        Column("afn_unsellable_quantity", Integer),
+        Column("afn_reserved_quantity", Integer),
+        Column("afn_total_quantity", Integer),
+        Column("afn_inbound_working_quantity", Integer),
+        Column("afn_inbound_shipped_quantity", Integer),
+        Column("afn_inbound_receiving_quantity", Integer),
+        Column("afn_researching_quantity", Integer),
+        Column("afn_reserved_future_supply", Integer),
+        Column("afn_future_supply_buyable", Integer),
+        Column("per_unit_volume", Float),
+        Column("last_updated", DateTime),
+    )
 
-# Create table if not exists
-metadata.create_all(engine, checkfirst=True)
+    # Create table if not exists
+    metadata.create_all(engine, checkfirst=True)
 
-# The data
-data = [
-    {
-        "sku": "HO-NTMR-XI7C",
-        "fnsku": "X001Q5REZZ",
-        "asin": "B07M5YWVVB",
-        "product_name": "Sotally Tober Drinking Games for Adults - Outrageously Fun Adult Party Card Game",
-        "condition": "New",
-        "your_price": 1499,
-        "mfn_listing_exists": False,
-        "mfn_fulfillable_quantity": None,
-        "afn_listing_exists": True,
-        "afn_warehouse_quantity": 112,
-        "afn_fulfillable_quantity": 112,
-        "afn_unsellable_quantity": 0,
-        "afn_reserved_quantity": 0,
-        "afn_total_quantity": 112,
-        "afn_inbound_working_quantity": 0,
-        "afn_inbound_shipped_quantity": 0,
-        "afn_inbound_receiving_quantity": 0,
-        "afn_researching_quantity": 0,
-        "afn_reserved_future_supply": 0,
-        "afn_future_supply_buyable": 0,
-        "per_unit_volume": 371.17,
-        "last_updated": datetime.now(),
-    }
-]
+    return None
 
-# Convert list of dict to dataframe
-df = pd.DataFrame(data)
+def create_orders():
+    orders_table = Table(
+    'orders', metadata,
+        Column('buyer_email', String(100)),
+        Column('amazon_order_id', String(50), primary_key=True),
+        Column('earliest_ship_date', DateTime),
+        Column('sales_channel', String(50)),
+        Column('order_status', String(20)),
+        Column('number_of_items_shipped', Integer),
+        Column('order_type', String(20)),
+        Column('is_premium_order', Boolean),
+        Column('is_prime', Boolean),
+        Column('fulfillment_channel', String(20)),
+        Column('number_of_items_unshipped', Integer),
+        Column('has_regulated_items', Boolean),
+        Column('is_replacement_order', Boolean),
+        Column('is_sold_by_ab', Boolean),
+        Column('latest_ship_date', DateTime),
+        Column('ship_service_level', String(20)),
+        Column('is_ispu', Boolean),
+        Column('marketplace_id', String(20)),
+        Column('purchase_date', DateTime),
+        Column('shipping_state_or_region', String(50)),
+        Column('shipping_postal_code', String(20)),
+        Column('shipping_city', String(50)),
+        Column('shipping_country_code', String(10)),
+        Column('is_access_point_order', Boolean),
+        Column('seller_order_id', String(50)),
+        Column('payment_method', String(20)),
+        Column('is_business_order', Boolean),
+        Column('currency_code', String(10)),
+        Column('amount', Float),
+        Column('payment_method_details', String(20)),
+        Column('is_global_express_enabled', Boolean),
+        Column('last_update_date', DateTime),
+        Column('shipment_service_level_category', String(20)),
+    )
 
-# Write data to PostgreSQL using DataFrame's to_sql() method
-df.to_sql("inventory", engine, if_exists="append", index=False)
+    metadata.create_all(engine, checkfirst=True)
+    return None
+
+
+if __name__ == "__main__":
+    # create_inventory()
+    create_orders()
--- tools/amazon/orders.py
+++ tools/amazon/orders.py
@@ -5,6 +5,19 @@ from keys import *
 from datetime import datetime, timedelta
 from dateutil.tz import tzlocal
 
+from sqlalchemy import (
+        create_engine,
+        MetaData,
+        Table,
+        Column,
+        Integer,
+        String,
+        Boolean,
+        Float,
+        DateTime,
+    )
+from sqlalchemy.dialects.postgresql import insert
+
 CLIENT_CONFIG = {
     "lwa_app_id": LWA_APP_ID,
     "lwa_client_secret": LWA_CLIENT_SECRET,
@@ -59,11 +72,142 @@ def orders_report(period: int):
         print("NextToken not found in this dictionary.")
     return metrics
 
+def upsert_orders(data):
+    print("Attempting to upsert orders")   
+
+    from keys import DB_USER, DB_PASSWORD
+
+    # Define your PostgreSQL connection parameters
+    db_user = DB_USER
+    db_password = DB_PASSWORD
+    db_host = "localhost"
+    db_port = "5432"
+    db_name = "amazonseller"  # Replace with your database name
+
+    # Create the SQLAlchemy engine
+    engine = create_engine(
+        f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+    )
+    metadata = MetaData()
+
+    # Define the orders_table
+    orders_table = Table('orders', metadata, autoload_with=engine)
+
+    for order in data['Orders']:
+        # Flatten the order data
+        if 'ShippingAddress' in order:
+            state_or_region = order['ShippingAddress'].get('StateOrRegion', None)
+            shipping_postal_code = order['ShippingAddress'].get('PostalCode', None)
+            shipping_city = order['ShippingAddress'].get('City', None)
+            shipping_country_code = order['ShippingAddress'].get('CountryCode', None)
+        else:
+            state_or_region = None
+        if 'OrderTotal' in order:
+            currency_code = order['OrderTotal'].get('currency_code', None)
+            amount = order['OrderTotal'].get('amount', 0)
+        else:
+            OrderTotal = None
+        order_data = {
+            'buyer_email': order['BuyerInfo'].get('BuyerEmail', None),
+            'amazon_order_id': order['AmazonOrderId'],
+            'earliest_ship_date': datetime.fromisoformat(order['EarliestShipDate'].replace("Z", "+00:00")),
+            'sales_channel': order['SalesChannel'],
+            'order_status': order['OrderStatus'],
+            'number_of_items_shipped': order['NumberOfItemsShipped'],
+            'order_type': order['OrderType'],
+            'is_premium_order': True if order['IsPremiumOrder'] == 'true' else False,
+            'is_prime': True if order['IsPrime'] == 'true' else False,
+            'has_regulated_items': True if order['HasRegulatedItems'] == 'true' else False,
+            'is_replacement_order': True if order['IsReplacementOrder'] == 'true' else False,
+            'is_sold_by_ab': True if order['IsSoldByAB'] == 'true' else False,
+            'is_ispu': True if order['IsISPU'] == 'true' else False,
+            'is_access_point_order': True if order['IsAccessPointOrder'] == 'true' else False,
+            'is_business_order': True if order['IsBusinessOrder'] == 'true' else False,
+            'is_global_express_enabled': True if order['IsGlobalExpressEnabled'] == 'true' else False,
+            'fulfillment_channel': order['FulfillmentChannel'],
+            'number_of_items_unshipped': order['NumberOfItemsUnshipped'],
+            'latest_ship_date': datetime.fromisoformat(order['LatestShipDate'].replace("Z", "+00:00")),
+            'ship_service_level': order['ShipServiceLevel'],
+            'marketplace_id': order['MarketplaceId'],
+            'purchase_date': datetime.fromisoformat(order['PurchaseDate'].replace("Z", "+00:00")),
+            'shipping_state_or_region': state_or_region,
+            'shipping_postal_code': shipping_postal_code,
+            'shipping_city': shipping_city,
+            'shipping_country_code': shipping_country_code,
+            'seller_order_id': order['SellerOrderId'],
+            'payment_method': amount,
+            'currency_code': currency_code,
+            'amount': float(amount),
+            'payment_method_details': order['PaymentMethodDetails'][0] if order['PaymentMethodDetails'] else None,
+            'last_update_date': datetime.fromisoformat(order['LastUpdateDate'].replace("Z", "+00:00")),
+            'shipment_service_level_category': order['ShipmentServiceLevelCategory']
+        }
+
+        # Create insert statement
+        stmt = insert(orders_table).values(**order_data)
+
+        # Specify the action to perform if a conflict occurs
+        stmt = stmt.on_conflict_do_update(index_elements=['amazon_order_id'],
+                                          set_=order_data
+                                          )
+        
+        # Open a session
+        with engine.begin() as connection:
+            # Try to insert the data
+            try:
+                connection.execute(stmt)
+            # If a conflict occurs
+            except Exception as e:
+                print(e)
+                # Update the existing row
+                connection.execute(
+                    orders_table.update()
+                    .where(orders_table.c.amazon_order_id == order_data['amazon_order_id'])
+                    .values(**order_data)
+                )
+
+        # Execute the statement
+        with engine.connect() as connection:
+            connection.execute(stmt)
+
+    return None
+
+def write_data_to_file(data):
+    # Define your file path
+    file_path = "/opt/documentation-helper/tools/amazon/responses/orders.json"
+
+    # Open the file in write mode
+    with open(file_path, 'w') as json_file:
+        # Use the json.dump method to write the data to the file
+        json.dump(data, json_file)
+    return None
+
+def read_data_from_file():
+    # Define your file path
+    file_path = "/opt/documentation-helper/tools/amazon/responses/orders.json"
+
+    # Open the file in read mode
+    with open(file_path, 'r') as json_file:
+        # Use the json.load method to read the data from the file
+        data = json.load(json_file)
+    return data
 
 if __name__ == "__main__":
-    data = orders_report(365)
+    
+    dev_mode=True
+    if dev_mode:
+        print("Running in Dev Mode")
+        data = read_data_from_file()
+    else:
+        print("Running in Live Mode")   
+        data = orders_report(365)
+        write_data_to_file(data)
 
-    print(len(data["Orders"]))
+    orders = len(data['Orders'])
+    canceled_orders = len([order for order in data["Orders"] if order["OrderStatus"] == "Canceled"])
+    total_orders = orders - canceled_orders
 
-    canceled = [order for order in data["Orders"] if order["OrderStatus"] == "Canceled"]
-    print(len(canceled))
+    print(f"Orders: {orders}")
+    print(f"Canceled Orders: {canceled_orders}")
+    print(f"Total Orders: {total_orders}")
+    upsert_orders(data)
